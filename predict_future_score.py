0e4efb09415dae53cae64691b604db0ea2e7adcb
hiddenSharp429 <z404878860@163.com>
2024-10-26 14:14:35 +0800

    🎨 Improve structure of the code
    better structure of 'predict_future_score.py' file

--- predict_future_score.py
+++ predict_future_score.py
@@ -2,8 +2,9 @@
 Author: hiddenSharp429 z404878860@163.com
 Date: 2024-10-26 13:27:45
 LastEditors: hiddenSharp429 z404878860@163.com
-LastEditTime: 2024-10-26 13:28:49
+LastEditTime: 2024-10-26 14:02:00
 '''
+import os
 import pandas as pd
 from src.data_processing.data_preparation import data_prepare, add_future_three_years_data, split_data_by_province
 from src.data_processing.missing_value_imputation import missing_values_Imputation_poly_regression
@@ -12,15 +13,25 @@ from src.models.model_evaluation import evaluate_model_performance, save_model, 
 from src.utils.visualization import scatter_plot, check_effect_of_poly_regression_Imputation
 import config
 
-def main():
-    # # Second part: prediction
-    # ## Add 2024 to 2026 data to the table and initialize it to 0, save as future_three_years_data_0.xlsx
+def predict_future_score():
+    # Second part: prediction
+    ## Add 2024 to 2026 data to the table and initialize it to 0, save as future_three_years_data_0.xlsx
     add_future_three_years_data(config.PROCESSED_DATA_PATH, config.OUTPUT_DATA0_PATH)
     
-    # ## Use ARIMA to fill the 2024 to 2026 data of each attribute column, and save the filled file as future_three_years_data.xlsx
+    ## Use ARIMA to fill the 2024 to 2026 data of each attribute column, and save the filled file as future_three_years_data.xlsx
+    # OUTPUT_DATA0 -> OUTPUT_DATA1
+
+
+    ## Use the newly filled table (future_three_years_data.xlsx) after adding the corresponding model, and then predict the green high-quality development indicators for the next three years
+    ## choose the existing model in the model folder
+    model_list = os.listdir(config.MODEL_PATH)
+    print("The existing models are: ", model_list)
+    pre_model_url = input("Please choose the model: ")
     
-    # ## Use the newly filled table (future_three_years_data.xlsx) after adding the corresponding model, and then predict the green high-quality development indicators for the next three years
-    prediction_future_three_years_HQ_score(config.OUTPUT_DATA1_PATH, config.OUTPUT_DATA2_PATH, pre_model_url=config.BEST_MODEL_PATH)
+    if pre_model_url in model_list:
+        prediction_future_three_years_HQ_score(config.OUTPUT_DATA1_PATH, config.OUTPUT_DATA2_PATH, pre_model_url=config.MODEL_PATH + "/" + pre_model_url)
+    else:
+        print("The model you chose does not exist, please try again.")
 
 if __name__ == "__main__":
-    main()
+    predict_future_score()
